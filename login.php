f41ad91e4c5be85f1bb99cb09eb21df7429eb636
tmantta <tmantta04@gmail.com>
2016-03-10 18:14:38 -0600

    login now works
    login now works

--- login.php
+++ login.php
@@ -1,38 +1,33 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"> 
-<html xmlns = "http://www.w3.org/1999/xhtml">
-
-<head>
-
-<link rel="stylesheet" type="text/css" href="mystyle.css"></link>
-    </script>
-<title>Login</title>
-
-
-
-</head>
-<body class="infoPage">
-
 <?php
-//start session
 session_start();
 // Open database connection
-$conn = mysqli_connect("localhost", "mantta2t", "winter15", "mantta2t");
+$conn = mysqli_connect("localhost", "streibeb_cs372rw", "urspace1", "streibeb_cs372");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
 
 // perform database query
-$sql = "SELECT * FROM users WHERE Email = '".$_POST["uName"]."' and Password = '".$_POST["pWord"]."';";
+$sql = "SELECT * FROM Users WHERE email = '".$_POST["uName"]."' and password = '".$_POST["pWord"]."';";
 $result = mysqli_query($conn, $sql);
 
 // if no results found
 if (mysqli_num_rows($result)<=0)
 {
 	//display error and close database
-echo "Invalid username and/or password.<br/>Redirecting...";
+echo	'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"> 
+<html xmlns = "http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" type="text/css" href="mystyle.css"></link>
+    </script>
+<title>Login</title>
+</head>
+<body class="infoPage">
+Invalid username and/or password.<br/>Redirecting...
+</body>
+</html>';
 mysqli_close($conn);
-header( "refresh:3;url=index.html");
+echo ' <META HTTP-EQUIV="Refresh" Content="3; URL=index.html"> ';
 
 
 }else{
@@ -44,20 +39,23 @@ mysqli_close($conn);
 // Store Session Data
 $_SESSION['login_user']= $_POST['uName'];
 
-echo "Login Successful.<br/>Redirecting...";
+echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"> 
+<html xmlns = "http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" type="text/css" href="mystyle.css"></link>
+    </script>
+<title>Login</title>
+</head>
+<body class="infoPage">
+Login Successful.<br/>Redirecting...
+</body>
+</html>';
 // redirect
-header( "refresh:3;url=wall.php");
+echo '   <META HTTP-EQUIV="Refresh" Content="3; URL=wall.php"> ';
 exit();
 }
-
-
-
-
-
-
 ?>
 
 
 
-</body>
-</html>
