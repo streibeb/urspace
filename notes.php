bb03bf930b21c8b133c617148603f108421d5d10
rooshan1234 <itsrooshan@gmail.com>
2016-03-19 00:11:16 -0600

    Fixed notes.php loading errors
    TODO: needs PHP populatenotelist.php sanitization

--- notes.php
+++ notes.php
@@ -8,10 +8,8 @@ if(!isset($_SESSION['login_user']))
 
 }
 // include function to add hashtags
-include 'bonus.php';
 
 ?>
-
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
 	"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
