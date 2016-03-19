bdd701321dd7e60c5f7a4f98519a25f4c2f89af9
Brayden Streibel <Vince@Tali.local>
2016-03-19 17:14:20 -0600

    Updated post.php

--- post.php
+++ post.php
@@ -1,17 +1,74 @@
 <?php
 //start session
 session_start();
+include_once("config.php");
+
 // if user not logged in, redirect to homepage
 if(!isset($_SESSION['login_user']))
 {
 	header('Location: index.html');
-	
+	exit();
 }
 
+if (isset($_POST["submit"])) {
+	// Open database connection
+	$conn = mysqli_connect(DB_HOST_NAME, DB_USER, DB_PASS, DB_NAME);
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+
+	$uniqueId = uniqid();
+	$uploaded = false;
+
+	$tempFile = USER_UPLOAD_DIRECTORY . basename($_FILES["postPic"]["name"]);
+	$ext = pathinfo($tempFile, PATHINFO_EXTENSION);
+	$target_file = USER_UPLOAD_DIRECTORY . $uniqueId . "." . $ext;
+
+	//error check image upload
+	if ($tempFile == USER_UPLOAD_DIRECTORY)// if user did not upload file
+	{
+		// do nothing
+	} else if (!preg_match("/(\.jpg|gif|png|jpeg)/",$target_file)) { // if file extension is bad
+		$err =  "Image must be png, gif, jpg or jpeg format."; // output error message
+	} else {
+		if (move_uploaded_file($_FILES["postPic"]["tmp_name"], $target_file)) {
+			//echo basename( $_FILES["postPic"]["name"]). " has been uploaded.";
+			$uploaded = true;
+		} else {
+			$err = "Error uploading image to database.";
+		}
+	}
+
+	$uid = $_SESSION['login_user'];
+	$content = htmlspecialchars(addslashes(trim($_POST['post1'])));
+	$date = date('Y/m/d H:i:s', time());
+
+	// if user uploaded image and link
+	if ($uploaded) {
+		//user uploaded image
+		$sql = "INSERT INTO Posts (creatorId, text, uploadedFile, timestamp)
+		VALUES ('$uid','$content','$target_file','$date');";
+	} else {// user has not uploaded image
+		$sql = "INSERT INTO Posts (creatorId, text, timestamp)
+		VALUES ('$uid','$content','$date');";
+	}
+
+	//upload post to database
+	if (!mysqli_query($conn, $sql)) {
+		$err = mysql_error($conn);
+	}
+
+	// close database connection
+	mysqli_close($conn);
+
+	if (isset($err)) {
+		header('Location: wall.php');
+	}
+}
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"> 
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns = "http://www.w3.org/1999/xhtml">
 
 <head>
@@ -56,7 +113,8 @@ if(!isset($_SESSION['login_user']))
 
 			<div class="col-xs-10 col-md-6 col-md-offset-2"> <!-- Content column !-->
 				<div class="signupSection">
-					<form action="posted.php" method="POST" enctype="multipart/form-data" id="postForm">
+					<?php if (isset($err)) echo "<p>An error has occurred.<br/>'$err'</p>" ?>
+					<form action="post.php" method="POST" enctype="multipart/form-data" id="postForm">
 						<fieldset class="largeColorsec">
 							<legend>New Anonymous Post</legend>
 
@@ -65,10 +123,10 @@ if(!isset($_SESSION['login_user']))
 							Post: <br></br><textarea class="textBox" name="post1" rows="7" cols="100"></textarea>
 							<br></br><span class="errorMsg" id="errorComments"></span>
 
-							   <p>
-							  <input type="submit" value="Submit"/>
-							  <input type="reset" value="Reset"/>
-							  </p>
+							<p>
+								<input type="submit" name="submit" value="Submit"/>
+								<input type="reset" value="Reset"/>
+							</p>
 						</fieldset>
 					</form>
 				</div>
