ac0b9f1eab94c4a14fa2d24744f7df24e2f03dba
Brayden Streibel <Vince@Tali.local>
2016-03-19 19:30:16 -0600

    Uploading images should work again

--- post.php
+++ post.php
@@ -22,12 +22,13 @@ if (isset($_POST["submit"])) {
 	$uniqueId = uniqid();
 	$uploaded = false;
 
-	$tempFile = USER_UPLOAD_DIRECTORY . basename($_FILES["postPic"]["name"]);
+	$tempFile = USER_IMAGE_UPLOAD_DIRECTORY . basename($_FILES["postPic"]["name"]);
 	$ext = pathinfo($tempFile, PATHINFO_EXTENSION);
-	$target_file = USER_UPLOAD_DIRECTORY . $uniqueId . "." . $ext;
+	$target_file = USER_IMAGE_UPLOAD_DIRECTORY . $uniqueId . "." . $ext;
+	$filename = $uniqueId . "." . $ext;
 
 	//error check image upload
-	if ($tempFile == USER_UPLOAD_DIRECTORY)// if user did not upload file
+	if ($tempFile == USER_IMAGE_UPLOAD_DIRECTORY)// if user did not upload file
 	{
 		// do nothing
 	} else if (!preg_match("/(\.jpg|gif|png|jpeg)/",$target_file)) { // if file extension is bad
@@ -49,7 +50,7 @@ if (isset($_POST["submit"])) {
 	if ($uploaded) {
 		//user uploaded image
 		$sql = "INSERT INTO Posts (creatorId, text, uploadedFile, timestamp)
-		VALUES ('$uid','$content','$target_file','$date');";
+		VALUES ('$uid','$content','$filename','$date');";
 	} else {// user has not uploaded image
 		$sql = "INSERT INTO Posts (creatorId, text, timestamp)
 		VALUES ('$uid','$content','$date');";
@@ -102,14 +103,8 @@ if (isset($_POST["submit"])) {
 					<a class="buttons" href="wall.php">View Posts</a>
 					<p class="blankButton">New Post</p>
 					<a class="buttons" href="search.php">Search</a>
+					<a class="buttons" href="admin.php">Admin</a>
 					<a class="buttons" href="logout.php">Logout</a>
-					<br></br>
-					<img src="advertisment1.jpg" class="ad" alt="ad1"></img>
-					<img src="advertisment2.jpg" class="ad" alt="ad2"></img>
-					<img src="advertisment3.jpg" class="ad" alt="ad3"></img>
-					<img src="advertisment4.jpg" class="ad" alt="ad4"></img>
-					<img src="advertisment5.jpg" class="ad" alt="ad5"></img>
-
 				</div>
 			</div>
 
